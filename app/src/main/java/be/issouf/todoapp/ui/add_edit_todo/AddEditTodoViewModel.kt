61e4c0796f5ee9aa25261e1d24a2390b324a89bf
is109109 <issoufagbello@gmail.com>
2023-07-25 20:24:02 +0200

    +view end

--- app/src/main/java/be/issouf/todoapp/ui/add_edit_todo/AddEditTodoViewModel.kt
+++ app/src/main/java/be/issouf/todoapp/ui/add_edit_todo/AddEditTodoViewModel.kt
@@ -57,7 +57,7 @@ class AddEditTodoViewModel @Inject constructor(
             }
             is AddEditTodoEvent.OnSaveTodoClick -> {
                 viewModelScope.launch {
-                    if (title.isNotBlank()){
+                    if (title.isBlank()){
                         sendUiEvent(UiEvent.ShowSnackbar(
                             message = "Le titre  ne doit pas être vide"
                         ))
